28fbce4f5222f09405c75d58fb7a9b8bc2a1a080
wangyiqi0409 <161693822+wangyiqi0409@users.noreply.github.com>
2024-03-14 21:15:05 +0800

    exercise in class 2024/3/14

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayHello(){
+  var response=prompt('What is love?');
+  alert('Yeah,'+response+',have a nice day!')
+}
+function showTopic(){
+  var x =document.getElementById('demo');
+  x.style.fontSize='25px';
+  x.style.color='red';
+}
